117bc6e2a97d2ce0a2b8ad9a9098059b85562d89
Cristian Marin <cristiancamilomarinflorez@gmail.com>
2021-08-31 15:25:47 -0500

    inicio JS

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+const $listaItem = document.querySelectorAll(".item");
+let $item = null;
+
+//Funcion para mostrar respuesta
+const solved = ($item) => {
+
+
+}
+
+//Funcion para encontrar el elemento activo
+const find = () => {
+
+}
